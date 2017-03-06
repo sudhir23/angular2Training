3948a0d09e2f39be6e0c3153666ae8ba8c941f21
FranzZemen <FranzZemen@gmail.com>
2017-03-06 09:06:07 -0600

    bootstrap

---
+++ epic1-fundamentals/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import './rxjs-operators';
+
+@Component({
+  selector: 'ocr-express',
+  templateUrl: 'app/app.component.html',
+  providers: [
+  ]
+})
+export class AppComponent {
+  constructor() {}
+}
+
+
---
+++ epic1-fundamentals/config/local.env.sample.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Franz on 3/6/2017.
+ */
+
+var config = {
+  indexHtmlBase: '/',
+  operatingMode: {
+    mode: 'DebugNoServer', // or 'Development', or 'Production'
+    simulatedRestTimeout: '1000'
+  },
+  logging: {
+    trace: 'false' // or 'true'
+  },
+  sass: {
+    includePaths: 'node_modules/foundation-sites/scss'
+  },
+  server : {
+    port: 8080,
+    host: 'localhost',
+    proxyHost: 'localhost',
+    proxyPort: '9000',
+    restBasePath: 'api/'
+  }
+  browser: 'windows',
+  browserConfig: {
+    osx: '/Applications/Google\ Chrome.app',
+    windows: 'chrome'
+  }
+};
+
+module.exports = config;
